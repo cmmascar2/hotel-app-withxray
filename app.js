5dfeb90b357c6ae886cd5bd35066d6c7390ac217
Yiming Peng <91288087+yimipeng@users.noreply.github.com>
2022-05-07 14:48:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use('/add', addRouter);
 app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
