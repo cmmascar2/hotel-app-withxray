a1a9ef4887495703c8af86e4de04a831b0af2240
Pradeep Ravichandran <ravicpra@amazon.com>
2022-05-07 13:01:56 -0700

    Add UI files for demo

--- routes/add.js
+++ routes/add.js
@@ -3,27 +3,27 @@ var router = express.Router();
 var rds=require('../rds');
 
 /* Add a new room */
-router.post('/', function(req, res, next) {
-  if (req.query.id && req.query.floor && req.query.hasView) {
-    console.log('New room request received. id: %s, floor: %s, hasView: %s', req.query.id, req.query.floor, req.query.hasView);
+router.post('/', function (req, res, next) {
+  if (req.body.roomNumber && req.body.floorNumber && req.body.hasView) {
+    console.log('New room request received. roomNumber: %s, floorNumber: %s, hasView: %s', req.body.roomNumber, req.body.floorNumber, req.body.hasView);
     rds.pool.getConnection(function(err, con){
       con.release();
       if (err) throw err;
 
-      con.query(`INSERT INTO hotel.rooms (id, floor, hasView) VALUES ('${req.query.id}', '${req.query.floor}', '${req.query.hasView}')`, function(err, result, fields) {
+      con.query(`INSERT INTO hotel.rooms (id, floor, hasView) VALUES ('${req.body.roomNumber}', '${req.body.floorNumber}', '${req.body.hasView}')`, function(err, result, fields) {
           if (err) res.send(err);
-          if (result) res.send({roomId: req.query.id, floor: req.query.floor, hasView: req.query.hasView});
+          if (result) res.send({roomId: req.body.roomNumber, floor: req.body.floorNumber, hasView: req.body.hasView});
           if (fields) console.log(fields);
       });
     });
   } else {
     console.log('Missing room id, floor or has view parameters');
-    res.send('Missing room id, floor or has view parameters');
+    throw new Error('Missing room id, floor or has view parameters');
   }
 });
 
 router.get('/', function(req, res, next) {
-    res.render('add', { title: 'Add new room' });
+    res.render('add', { title: 'Add new room', view: 'No' });
 });
 
 module.exports = router;
