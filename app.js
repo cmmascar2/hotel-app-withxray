a7c5622ffc514544698a071b922f207aa147f5fd
Yiming Peng <91288087+yimipeng@users.noreply.github.com>
2022-05-07 17:41:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -41,10 +41,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  if (err.status != 404) {
-    res.status(err.status || 500);
-    res.render('error');
-  }
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
